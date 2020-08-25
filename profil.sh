c289fb014f72f37e29133b4337af51e92b061a04
habibilah1701 <64044793+habibilah1701@users.noreply.github.com>
2020-08-25 21:17:13 +0700

    Add files via upload

---
+++ profil.sh
@@ -0,0 +1,68 @@
+Skip to content
+Learn Git and GitHub without any code!
+Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
+
+
+potatonc
+/
+ScriptAutoInstallPotato
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+ScriptAutoInstallPotato/menu/profile.sh
+@potatonc
+potatonc Add files via upload
+ 1 contributor
+34 lines (32 sloc)  1.87 KB
+ 
+# ~/.profile: executed by Bourne-compatible login shells.
+
+if [ "$BASH" ]; then
+  if [ -f ~/.bashrc ]; then
+    . ~/.bashrc
+  fi
+fi
+
+mesg n || true
+clear
+echo "   '########:::'#######::'########::::'###::::'########::'#######::" | lolcat
+echo "    ##.... ##:'##.... ##:... ##..::::'## ##:::... ##..::'##.... ##:" | lolcat
+echo "    ##:::: ##: ##:::: ##:::: ##:::::'##:. ##::::: ##:::: ##:::: ##:" | lolcat
+echo "    ########:: ##:::: ##:::: ##::::'##:::. ##:::: ##:::: ##:::: ##:" | lolcat
+echo "    ##.....::: ##:::: ##:::: ##:::: #########:::: ##:::: ##:::: ##:" | lolcat
+echo "    ##:::::::: ##:::: ##:::: ##:::: ##.... ##:::: ##:::: ##:::: ##:" | lolcat
+echo "    ##::::::::. #######::::: ##:::: ##:::: ##:::: ##::::. #######::" | lolcat
+echo "   ..::::::::::.......::::::..:::::..:::::..:::::..::::::.......:::" | lolcat
+echo "   ================================================================" | lolcat
+echo "       Selamat datang kembali $HOSTNAME" | lolcat
+echo ""
+echo "         ####################################################      " | lolcat
+echo "         #   Perintah :                                     #      " | lolcat
+echo "         #       menu   -   Untuk menampilkan daftar menu   #      " | lolcat
+echo "         #                                                  #      " | lolcat
+echo "         #   Info/Bug report :                              #      " | lolcat
+echo "         #       Telegram   -   @akunsshprabayar                     #      " | lolcat
+echo "         #       watshapp  -   081374452477                   #      " | lolcat
+echo "         #                                                  #      " | lolcat
+echo "         #                Credit by babangboy                 #      " | lolcat
+echo "         ####################################################      " | lolcat
+echo ""
+echo "   ================================================================" | lolcat
+echo ""
+© 2020 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
